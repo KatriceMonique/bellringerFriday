0b5a14f1e3383c6f7834a80d31441862ab522500
Katrice White <kwhite@chicagotechacademy.org>
2013-11-21 14:51:16 -0600

    Things

--- sum_diff_product.rb
+++ sum_diff_product.rb
@@ -1,4 +1,24 @@
-def method(punk,boss)
-puts "into interger"
-punk = gets.chomp.to_i
-boss = gets.chomp.to_i
+class Trial
+      puts "Please insert a number"
+    answer = gets.chomp
+     puts "Place another number"
+      second = gets.chomp
+      
+      puts answer.to_i * second.to_i
+      puts answer.to_i - second.to_i
+      puts answer.to_i + second.to_i
+    
+    def initialize(number, savior)
+        @number = number
+        @savior = savior
+end
+
+    attr_accessor 'number', 'savior'
+
+end
+
+times = Trial.new '9', 9
+times.number # =>
+times.savior # =>
+
+puts result.product
